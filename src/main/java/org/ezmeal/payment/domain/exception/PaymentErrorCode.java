cdac9adea88c86077aa587e98d753569eecc73c9
HOHOKIM <164991647+HOHOKIM@users.noreply.github.com>
2026-04-28 11:50:26 +0900

    feat: 결제 api 생성, 결제시 상황에 있을수도있는 추가 error 코드 추가

--- src/main/java/org/ezmeal/payment/domain/exception/PaymentErrorCode.java
+++ src/main/java/org/ezmeal/payment/domain/exception/PaymentErrorCode.java
@@ -11,7 +11,7 @@ public enum PaymentErrorCode implements ErrorCode {
 
     PAYMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "PAY-001", "결제 내역을 찾을 수 없습니다."),
     PAYMENT_ALREADY_COMPLETED(HttpStatus.BAD_REQUEST, "PAY-002", "이미 완료된 결제입니다."),
-    PG_API_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "PAY-003", "PG사 통신 중 오류가 발생했습니다!.");
+    PG_API_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "PAY-003", "PG사 통신 중 오류가 발생했습니다.");
 
     private final HttpStatus status;
     private final String code;
