8f8023925e94bc6bbd7a0164415516ddc662dfec
HOHOKIM <164991647+HOHOKIM@users.noreply.github.com>
2026-04-30 15:05:21 +0900

    feat : common 수정사항 완료 결제 진행시 orderId 없이 x-User-ID 로 넘겨준값이 결제자라는 검증로직 생성

--- src/main/java/org/ezmeal/payment/application/service/PaymentService.java
+++ src/main/java/org/ezmeal/payment/application/service/PaymentService.java
@@ -3,20 +3,23 @@ package org.ezmeal.payment.application.service;
 import java.util.List;
 import java.util.UUID;
 import org.ezmeal.payment.application.dto.request.PaymentRequestDto;
+import org.ezmeal.payment.application.dto.request.TossConfirmRequest;
 import org.ezmeal.payment.application.dto.response.PaymentResponseDto;
 
 public interface PaymentService {
     // 1. 결제 요청 생성 (주문 시 호출)
-    PaymentResponseDto createPayment(PaymentRequestDto requestDto);
+    PaymentResponseDto createPayment(PaymentRequestDto requestDto, UUID authenticatedUserId);
 
     // 2. 결제 승인 처리 (PG사 인증 완료 후 호출)
-    PaymentResponseDto approvePayment(String paymentKey, UUID orderId, Integer amount);
+    PaymentResponseDto approvePayment(TossConfirmRequest request, UUID currentUserId);
 
     //  3. 결제 단건 조회
     PaymentResponseDto getPaymentDetail(UUID paymentId);
 
     //  4. 결제 목록 조회
     List<PaymentResponseDto> getPaymentList();
+
+
 }
 
 
