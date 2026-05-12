af7f122c57994406e26794457ff253a21c0194b2
HOHOKIM <164991647+HOHOKIM@users.noreply.github.com>
2026-05-12 10:32:39 +0900

    fix- "  common 0.1.3 으로 수정후 코드 리팩토링"

--- src/main/java/org/ezmeal/payment/PaymentApplication.java
+++ src/main/java/org/ezmeal/payment/PaymentApplication.java
@@ -13,9 +13,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableFeignClients
 
-@SpringBootApplication(scanBasePackages = {"com.ezmeal.payment", "com.ezmeal.common"})
-@EntityScan(basePackages = {"com.ezmeal.payment", "com.ezmeal.common"})
-@EnableJpaRepositories(basePackages = {"com.ezmeal.payment", "com.ezmeal.common"})
+@SpringBootApplication(scanBasePackages = {"org.ezmeal.payment", "com.ezmeal.common"})
+@EntityScan(basePackages = {"org.ezmeal.payment", "com.ezmeal.common"})
+@EnableJpaRepositories(basePackages = {"org.ezmeal.payment", "com.ezmeal.common"})
 public class PaymentApplication {
 
     public static void main(String[] args) {
